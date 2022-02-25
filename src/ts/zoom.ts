f7c85e619d9f222022ac7be5e432c5b5052f33de
marshallku <marshall@kakao.com>
2022-02-25 12:42:52 +0900

    Move getAverageRGB to utils dir

--- src/ts/zoom.ts
+++ src/ts/zoom.ts
@@ -1,3 +1,5 @@
+import getAverageRGB from "./utils/getAverageRGB";
+
 export default function Zoom(
     target: TAllowedTargets,
     options: {
@@ -134,51 +136,6 @@ export default function Zoom(
 
     const handleClick = (event: MouseEvent) => {
         zoom(event.target as HTMLImageElement);
-    };
-
-    /*
-     *
-     *  Source of the code below
-     *  https://stackoverflow.com/a/2541680
-     *
-     */
-    const getAverageRGB = (img: HTMLImageElement) => {
-        const blockSize = 5;
-        const rgb = { r: 0, g: 0, b: 0 };
-        const canvas = document.createElement("canvas");
-        const ctx = canvas.getContext("2d");
-        if (!ctx) return null;
-        const width = (canvas.width =
-            img.naturalWidth || img.offsetWidth || img.width);
-        const height = (canvas.height =
-            img.naturalHeight || img.offsetHeight || img.height);
-
-        let data;
-        let i = -4;
-        let length;
-        let count = 0;
-
-        ctx.drawImage(img, 0, 0);
-
-        try {
-            data = ctx.getImageData(0, 0, width, height);
-        } catch (e) {
-            return null;
-        }
-
-        length = data.data.length;
-        while ((i += blockSize * 4) < length) {
-            ++count;
-            rgb.r += data.data[i];
-            rgb.g += data.data[i + 1];
-            rgb.b += data.data[i + 2];
-        }
-
-        rgb.r = Math.floor(rgb.r / count);
-        rgb.g = Math.floor(rgb.g / count);
-        rgb.b = Math.floor(rgb.b / count);
-
-        return rgb;
     };
 
     const attach = (target: TAllowedTargets) => {
