3aec2618885b8e629682a4185fce0d7d063b7051
marshallku <marshall@kakao.com>
2022-02-25 13:31:14 +0900

    Add options interface

--- src/@types/zoom.d.ts
+++ src/@types/zoom.d.ts
@@ -1,1 +1,6 @@
 declare type TAllowedTargets = string | HTMLElement | NodeList | HTMLElement[];
+
+interface IZoomOptions {
+    background?: string;
+    onTransitionEnd?: (img: HTMLImageElement) => void;
+}
